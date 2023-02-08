c92b96dab2900794381bdaedccde45b16c8dd892
Eduardo Segredo <esegredo@ull.edu.es>
2023-02-08 21:58:14 +0000

    Some tests with mocha+chai added

--- src/basicFunctions.ts
+++ src/basicFunctions.ts
@@ -7,7 +7,7 @@
  * add(1, 7) = 8
  * ```
  */
-function add(firstNumber: number, secondNumber: number) {
+export function add(firstNumber: number, secondNumber: number) {
   return firstNumber + secondNumber;
 }
 
